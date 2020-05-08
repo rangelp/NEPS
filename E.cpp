54ee7ffdc5e2d66a4342a0004538ce42ec088e47
Diego Rangel Piranga Costa <diegorang@outlook.com.br>
2020-05-07 21:53:06 -0300

    Create E.cpp

---
+++ E.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (int) x.size()
+#define debug(x) cout << "DEBUG: " << x << endl;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair <int, int> ii;
+
+#define maxn 100005
+
+struct node{
+  ll xor_seq, or_seq, and_seq;
+  int mn, mx;
+  node(){}
+  node(ll xor_seq, ll or_seq, ll and_seq, int mn, int mx){
+    this->xor_seq = xor_seq;
+    this->or_seq = or_seq;
+    this->and_seq = and_seq;
+    this->mn = mn;
+    this->mx = mx;
+  }
+};
+
+node segtree[4*maxn];
+int arr[maxn];
+
+int min(int a, int b){
+  return arr[a] <= arr[b] ? a : b;
+}
+
+int max(int a, int b){
+  return arr[a] >= arr[b] ? a : b;
+}
+
+node combine(node a, node b){
+  return node(a.xor_seq^b.xor_seq, a.or_seq|b.or_seq, a.and_seq&b.and_seq, min(a.mn, b.mn), max(a.mx, b.mx));
+}
+
+void build(int i, int l, int r){
+  if(l == r){
+    segtree[i] = node(arr[l], arr[l], arr[l], l, l);
+    return;
+  }
+  int md = (l+r)/2, nxt = 2*i;
+  build(nxt, l, md);
+  build(nxt+1, md+1, r);
+  segtree[i] = combine(segtree[nxt], segtree[nxt+1]);
+}
+
+void update(int i, int l, int r, int p, int var){
+  if(l > p or r < p){
+    return;
+  }
+  if(l == p and r == p){
+    arr[p] = var;
+    segtree[i] = node(arr[p], arr[p], arr[p], p, p);
+    return;
+  }
+  int md = (l+r)/2, nxt = 2*i;
+  update(nxt, l, md, p, var);
+  update(nxt+1, md+1, r, p, var);
+  segtree[i] = combine(segtree[nxt], segtree[nxt+1]);
+}
+
+node query(int i, int l, int r, int x, int y){
+  if(l > y or r < x){
+    return node(0, 0, (1LL<<30)-1, maxn-1, maxn-2);
+  }
+  if(l >= x and r <= y){
+    return segtree[i];
+  }
+  int md = (l+r)/2, nxt = 2*i;
+  return combine(query(nxt, l, md, x, y), query(nxt+1, md+1, r, x, y));
+}
+
+int main(){
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  int n, q;
+  cin >> n >> q;
+  for(int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+  arr[maxn-1] = (1LL<<30);
+  arr[maxn-2] = -(1LL<<30);
+  build(1, 0, n-1);
+  while(q--){
+    char op;
+    int x, y;
+    cin >> op >> x >> y;
+    if(op == 'x'){
+      node a = query(1, 0, n-1, x-1, y-1);
+      cout << (a.xor_seq == 0 ? arr[a.mx] : -1) << "\n";
+    }
+    else if(op == 'o'){
+      int id = query(1, 0, n-1, x-1, y-1).mx;
+      ll L = query(1, 0, n-1, x-1, id-1).or_seq, R = query(1, 0, n-1, id+1, y-1).or_seq;
+      ll OR = L|R;
+      cout << (OR == arr[id] ? arr[id] : -1) << "\n";
+    }
+    else if(op == 'a'){
+      int id = query(1, 0, n-1, x-1, y-1).mn;
+      ll L = query(1, 0, n-1, x-1, id-1).or_seq, R = query(1, 0, n-1, id+1, y-1).or_seq;
+      ll AND = L&R;
+      cout << (AND == arr[id] ? arr[id] : -1) << "\n";
+    }
+    else{
+      update(1, 0, n-1, x-1, y);
+    }
+  }
+  return 0;
+}
