4f7c97a199cf915d229468c7180cfbc0e357db94
Diego Rangel Piranga Costa <diegorang@outlook.com.br>
2020-05-13 21:58:21 -0300

    Update E.cpp

--- E.cpp
+++ E.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-#define all(x) x.begin(), x.end()
+#define aint(x) x.begin(), x.end()
 #define sz(x) (int) x.size()
 #define debug(x) cout << "DEBUG: " << x << endl;
 
-typedef long long ll;
+typedef long long int ll;
 typedef unsigned long long ull;
 typedef pair <int, int> ii;
 
-#define maxn 100005
+#define maxn 100040
 
 struct node{
   ll xor_seq, or_seq, and_seq;
@@ -26,18 +26,18 @@ struct node{
 };
 
 node segtree[4*maxn];
-int arr[maxn];
+ll arr[maxn];
 
-int min(int a, int b){
-  return arr[a] <= arr[b] ? a : b;
+int _min(int a, int b){
+  return arr[a] < arr[b] ? a : b;
 }
 
-int max(int a, int b){
-  return arr[a] >= arr[b] ? a : b;
+int _max(int a, int b){
+  return arr[a] > arr[b] ? a : b;
 }
 
 node combine(node a, node b){
-  return node(a.xor_seq^b.xor_seq, a.or_seq|b.or_seq, a.and_seq&b.and_seq, min(a.mn, b.mn), max(a.mx, b.mx));
+  return node(a.xor_seq^b.xor_seq, a.or_seq|b.or_seq, a.and_seq&b.and_seq, _min(a.mn, b.mn), _max(a.mx, b.mx));
 }
 
 void build(int i, int l, int r){
@@ -56,8 +56,8 @@ void update(int i, int l, int r, int p, int var){
     return;
   }
   if(l == p and r == p){
-    arr[p] = var;
-    segtree[i] = node(arr[p], arr[p], arr[p], p, p);
+    arr[l] = var;
+    segtree[i] = node(arr[l], arr[l], arr[l], l, l);
     return;
   }
   int md = (l+r)/2, nxt = 2*i;
@@ -68,7 +68,7 @@ void update(int i, int l, int r, int p, int var){
 
 node query(int i, int l, int r, int x, int y){
   if(l > y or r < x){
-    return node(0, 0, (1LL<<30)-1, maxn-1, maxn-2);
+    return node(0, 0, -1, maxn-1, maxn-2);
   }
   if(l >= x and r <= y){
     return segtree[i];
@@ -85,31 +85,46 @@ int main(){
   for(int i = 0; i < n; i++){
     cin >> arr[i];
   }
-  arr[maxn-1] = (1LL<<30);
-  arr[maxn-2] = -(1LL<<30);
+  arr[maxn-1] = (1LL<<56);
+  arr[maxn-2] = -(1LL<<56);
   build(1, 0, n-1);
   while(q--){
     char op;
     int x, y;
     cin >> op >> x >> y;
+    x--, y--;
     if(op == 'x'){
-      node a = query(1, 0, n-1, x-1, y-1);
+      node a = query(1, 0, n-1, x, y);
       cout << (a.xor_seq == 0 ? arr[a.mx] : -1) << "\n";
     }
     else if(op == 'o'){
-      int id = query(1, 0, n-1, x-1, y-1).mx;
-      ll L = query(1, 0, n-1, x-1, id-1).or_seq, R = query(1, 0, n-1, id+1, y-1).or_seq;
+      int id = query(1, 0, n-1, x, y).mx;
+      ll L = 0, R = 0;
+      if(id != x){
+        L = query(1, 0, n-1, x, id-1).or_seq;
+      }
+      if(id != y){
+        R = query(1, 0, n-1, id+1, y).or_seq;
+      }
+      //L = query(1, 0, n-1, x, id-1).or_seq, R = query(1, 0, n-1, id+1, y).or_seq;
       ll OR = L|R;
       cout << (OR == arr[id] ? arr[id] : -1) << "\n";
     }
     else if(op == 'a'){
-      int id = query(1, 0, n-1, x-1, y-1).mn;
-      ll L = query(1, 0, n-1, x-1, id-1).or_seq, R = query(1, 0, n-1, id+1, y-1).or_seq;
+      int id = query(1, 0, n-1, x, y).mn;
+      ll L = -1, R = -1;
+      if(id != x){
+        L = query(1, 0, n-1, x, id-1).and_seq;
+      }
+      if(id != y){
+        R = query(1, 0, n-1, id+1, y).and_seq;
+      }
+      //L = query(1, 0, n-1, x, id-1).and_seq, R = query(1, 0, n-1, id+1, y).and_seq;
       ll AND = L&R;
       cout << (AND == arr[id] ? arr[id] : -1) << "\n";
     }
     else{
-      update(1, 0, n-1, x-1, y);
+      update(1, 0, n-1, x, y+1);
     }
   }
   return 0;
